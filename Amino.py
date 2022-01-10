c175cc6244be0322d3e419a72adcee939bcd2e97
Lrsno <97423376+Lrsno@users.noreply.github.com>
2022-01-10 07:25:25 +0300

    Add files via upload

---
+++ Amino.py
@@ -0,0 +1,46 @@
+import os
+os. system('clear')
+  
+print( "\033[4;31m                             _____   _____   _ _                              ")
+print( "\033[4;31m                            /  ___| /  _  \ |_  |                             ")
+print( "\033[4;31m                            | |___  | | | |   | |                             ")
+print( "\033[4;31m                            |  _  \ | |/| |   | |                             ")
+print( "\033[4;31m                            | |_| | | |_| |   | |                             ")
+print( "\033[4;31m                            \_____/ \_____/   |_|                             ")
+
+print("  Loks.601")
+
+hackk = input ("version type:")
+    
+if hackk == "3.4.33595" :
+    print("Wait for the counter.../")
+    
+else:
+    print("Download...!")
+    
+    import os
+    os.system("termux-open-url https://2u.pw/FAKta") 
+   
+hackko = input ("Input codeid:")
+
+if hackko == "jh1nfnf0efjk3bcs04bfwp3f" :
+    print("Amino.app")
+    import os
+    os.system("termux-open-url https://aminoapps.com/login")
+    print("error...")
+son=1000
+
+while son < 5000 :
+ son = son+1
+ print(son) 
+print("error...!") 
+
+if hackko == "jh1nfnf0efjk3bcs04bfwp3f" :
+    input ("beginning: ")
+    import os
+    os.system("termux-open-url https://n9.cl/pdjcy")    
+    input ("Are you done: ")
+    print("2000..")
+    print("4000..")
+    print("Within 24 hours you will receive the coins according to US time.")
+
